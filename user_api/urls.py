b6351e9c0f77dc79ed536184962a830914ca325a
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-11-12 23:48:19 +0100

    LoginViewset, filter_backends for search

--- user_api/urls.py
+++ user_api/urls.py
@@ -5,7 +5,9 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('profile', views.UserProfileViewSet)
+router.register('login', views.LoginViewset, base_name='login')
 
-urlpatterns = [
-    path('', include(router.urls)),
-]
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
+urlpatterns = router.urls
--- user_api/views.py
+++ user_api/views.py
@@ -2,6 +2,9 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from . import models
 from .serializers import UserProfileSerializer
@@ -13,3 +16,12 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication, )
     permission_classes = (permissions.UpdateOwnProfile, )
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('name', 'email')
+
+
+class LoginViewset(viewsets.ViewSet):
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request, *args, **kwargs):
+        return ObtainAuthToken().post(request)
