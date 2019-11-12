1e286128f1e48cda463f6e86613d9c8e5bda65b0
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-11-12 23:15:49 +0100

    added permissions

--- user_api/views.py
+++ user_api/views.py
@@ -1,11 +1,15 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 
 from . import models
 from .serializers import UserProfileSerializer
+from . import permissions
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (permissions.UpdateOwnProfile, )
