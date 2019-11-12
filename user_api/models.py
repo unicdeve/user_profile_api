30b37579b8bb318689c254aa62edb53126c3646b
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-11-12 23:59:34 +0100

    ProfileFeedItem model

--- user_api/models.py
+++ user_api/models.py
@@ -56,3 +56,13 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    user = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=200)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.status_text
